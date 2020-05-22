04f048d1090893977299b931f2e1ab7f8e4ea193
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-05-22 15:38:15 -0600

    change socket.io events to constants

--- server/socket/GameHandler.js
+++ server/socket/GameHandler.js
@@ -1,15 +1,13 @@
 const { v4: uuidv4 } = require("uuid");
+const GameInfo = require("./GameInfo");
 
 class GameHandler {
   constructor(io, player1, player2, socket1, socket2, showDebug = false) {
     this.gameId = this.generateGameId();
-    this.player1 = player1;
-    this.player2 = player2;
+    this.gameInfo = new GameInfo(player1, player2, this.generateGameId());
     this.socket1 = socket1;
     this.socket2 = socket2;
     this.io = io;
-    this.state = GameHandler.states.created;
-    this.winner = null;
     this.showDebug = showDebug;
 
     // bind these - passed as callback functions
@@ -29,94 +27,102 @@ class GameHandler {
   }
 
   initialize() {
-    if (this.state !== GameHandler.states.created) {
+    const { state, gameId } = this.gameInfo;
+
+    if (state !== GameInfo.states.created) {
       this.debug("Game already initialized");
       throw "Game already initialized";
     }
     // set sockets to join this new game room
-    this.socket1.join(this.gameId);
-    this.socket2.join(this.gameId);
+    this.socket1.join(gameId);
+    this.socket2.join(gameId);
 
     this.addListeners();
 
-    this.state = GameHandler.states.initialized;
+    this.gameInfo.state = GameInfo.states.initialized;
   }
 
   onDisconnect(disconnectedPlayer) {
+    const { player1, player2, gameId } = this.gameInfo;
+
     this.debug(
-      "Player " + this.player + " disconnected from game " + this.gameId
+      "Player " + disconnectedPlayer + " disconnected from game " + gameId
     );
-    const gameWinner =
-      disconnectedPlayer == this.player1 ? this.player2 : this.player1;
+    const gameWinner = disconnectedPlayer == player1 ? player2 : player1;
     this.finish(gameWinner);
   }
 
   // abstract to function to ensure correctly removed when game is finished
   onP1Disconnect() {
     this.debug("player 1 disconnected");
-    this.onDisconnect(this.player1);
+    this.onDisconnect(this.gameInfo.player1);
   }
 
   // abstract to function to ensure correctly removed when game is finished
   onP2Disconnect() {
     this.debug("player 2 disconnected");
-    this.onDisconnect(this.player2);
+    this.onDisconnect(this.gameInfo.player2);
   }
 
   onKeystroke(data) {
     // responds to keystroke from any player
     // emits to all players
     this.debug("Player " + data.username + " keystroke");
-    this.io.in(this.gameId).emit("keystroke", data);
+    this.io.in(this.gameInfo.gameId).emit(GameHandler.commands.KEYSTROKE, data);
   }
 
   addListeners() {
+    this.socket1.on(GameHandler.commands.KEYSTROKE, this.onKeystroke);
+    this.socket2.on(GameHandler.commands.KEYSTROKE, this.onKeystroke);
 
-    this.socket1.on("keystroke", this.onKeystroke);
-    this.socket2.on("keystroke", this.onKeystroke);
-
-    this.socket1.on("disconnect", this.onP1Disconnect);
-    this.socket2.on("disconnect", this.onP2Disconnect);
+    this.socket1.on(GameHandler.commands.DISCONNECT, this.onP1Disconnect);
+    this.socket2.on(GameHandler.commands.DISCONNECT, this.onP2Disconnect);
   }
 
   removeListeners() {
-    this.socket1.off("keystroke", this.onKeystroke);
-    this.socket2.off("keystroke", this.onKeystroke);
+    this.socket1.off(GameHandler.commands.KEYSTROKE, this.onKeystroke);
+    this.socket2.off(GameHandler.commands.KEYSTROKE, this.onKeystroke);
 
-    this.socket1.off("disconnect", this.onP1Disconnect);
-    this.socket2.off("disconnect", this.onP2Disconnect);
+    this.socket1.off(GameHandler.commands.DISCONNECT, this.onP1Disconnect);
+    this.socket2.off(GameHandler.commands.DISCONNECT, this.onP2Disconnect);
   }
 
   start() {
-    if (this.state !== GameHandler.states.initialized) {
+    const { state, gameId, player1, player2 } = this.gameInfo;
+
+    if (state !== GameInfo.states.initialized) {
       this.debug("Start method called before game was initialized");
       throw "Start method called before game was initialized";
     }
 
-    this.debug("Game " + this.gameId + " started");
+    this.debug("Game " + gameId + " started");
     // emit start to all users in game
-    this.io.to(this.gameId).emit("start", {
-      player1: this.player1,
-      player2: this.player2,
-      gameId: this.gameId,
+    this.io.to(gameId).emit(GameHandler.commands.START, {
+      player1: player1,
+      player2: player2,
+      gameId: gameId,
     });
   }
 
   finish(winner) {
-    this.debug("Game " + this.gameId + " ended");
-    this.debug("Game " + this.gameId + " winner: " + winner);
+    const { gameId } = this.gameInfo;
 
-    this.winner = winner;
+    this.debug("Game " + gameId + " ended");
+    this.debug("Game " + gameId + " winner: " + winner);
 
-    this.io.to(this.gameId).emit("finish", { winner: this.winner });
+    this.gameInfo.winner = winner;
 
-    this.state = GameHandler.states.finished;
+    this.io
+      .to(gameId)
+      .emit(GameHandler.commands.FINISH, { winner: this.gameInfo.winner });
+
+    this.gameInfo.state = GameInfo.states.finished;
 
     this.removeListeners();
 
     // remove sockets from previously created room
-    this.socket1.leave(this.gameId);
-    this.socket2.leave(this.gameId);
+    this.socket1.leave(gameId);
+    this.socket2.leave(gameId);
   }
 
   generateGameId() {
@@ -124,12 +130,11 @@ class GameHandler {
   }
 }
 
-GameHandler.commands = ["start", "stop", "keystroke"];
-GameHandler.states = {
-  created: "created",
-  initialized: "initialized",
-  playing: "playing",
-  finished: "finished",
+GameHandler.commands = {
+  START: "start",
+  FINISH: "finish",
+  KEYSTROKE: "keystroke",
+  DISCONNECT: "disconnect",
 };
 
 module.exports = GameHandler;
