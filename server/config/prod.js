a2c4746a588e951f0aace78a5ed3bbad6556b85a
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-05-31 00:55:34 -0600

    add keys

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  postgresURI: process.env.POSTGRES_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  githubClientID: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  cookieKey: process.env.COOKIE_KEY,
+};
