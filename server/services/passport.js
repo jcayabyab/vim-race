73b08146ea426312449a5e6f272af60401be859c
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-06-05 21:54:26 -0600

    migrate to sslip.io

--- server/services/passport.js
+++ server/services/passport.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "http://192.168.0.24.xip.io:3000/auth/google/callback",
+      callbackURL: "https://192-168-0-24.sslip.io:3000/auth/google/callback",
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
