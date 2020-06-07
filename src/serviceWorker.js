22be3dbc83ae8ba907f51f9b36b3b119977c0dbe
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-06-06 18:37:00 -0600

    add customer service worker logic

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,11 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      // custom service worker
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+      
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
---
+++ src/sw-build.js
@@ -0,0 +1,26 @@
+const workboxBuild = require("workbox-build");
+
+const buildSW = () => {
+  // The build is expected to fail if the
+  // sw install rules couldn't be generated.
+  // Add a catch block to handle this scenario.
+  return workboxBuild
+    .injectManifest({
+      swSrc: "src/sw-custom.js", // custom sw rule
+
+      swDest: "build/sw.js", // sw output file (auto-generated
+
+      globDirectory: "build",
+
+      globPatterns: ["**/*.{js,css,html,png,svg}"],
+
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 102,
+    })
+    .then(({ count, size, warnings }) => {
+      warnings.forEach(console.warn);
+      console.info(`${count} files will be precached,
+                  totaling ${size / (1024 * 1024)} MBs.`);
+    });
+};
+
+buildSW();
