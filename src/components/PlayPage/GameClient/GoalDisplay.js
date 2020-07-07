8634f10355daf0d8be2cf669395ff028b4c56e48
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-07-06 19:08:13 -0600

    move GoalDisplay to separate file

---
+++ src/components/PlayPage/GameClient/GoalDisplay.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const GoalBox = styled.code`
+  background-color: #212121;
+  border: 1px solid black;
+  border-radius: 3px;
+  font-family: "Lucida Console", "Consolas", monospace;
+  white-space: pre-wrap;
+  font-size: 9pt;
+  min-height: 300px;
+  width: 600px;
+  overflow: scroll;
+`;
+
+const GoalHeader = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+`;
+
+const GoalHeaderChild = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  &:first-child {
+    margin-right: auto;
+    justify-content: flex-start;
+  }
+
+  &:last-child {
+    margin-left: auto;
+    justify-content: flex-end;
+  }
+`;
+
+const ShowDiffButton = styled.button`
+  border: 4px double white;
+  background-color: transparent;
+  font-family: "Share Tech Mono", source-code-pro, Menlo, Monaco, Consolas,
+    "Courier New", monospace;
+  color: white;
+  cursor: pointer;
+  padding: 5px;
+  font-size: 12pt;
+  border-radius: 3px;
+`;
+
+const RedText = styled.span`
+  background-color: rgba(220, 50, 60, 0.6);
+`;
+
+const GreenText = styled.span`
+  background-color: rgba(50, 205, 50, 0.6);
+`;
+
+const GoalDisplay = ({ diff, goalText }) => {
+  const createDiffText = () => {
+    return (
+      <React.Fragment>
+        {diff.map((token, index) => {
+          if (token.removed) {
+            return <RedText key={index}>{token.value}</RedText>;
+          }
+          if (token.added) {
+            return <GreenText key={index}>{token.value}</GreenText>;
+          }
+          return <span>{token.value}</span>;
+        })}
+      </React.Fragment>
+    );
+  };
+
+  const [showDiff, setShowDiff] = useState(false);
+  return (
+    <React.Fragment>
+      <GoalHeader>
+        <GoalHeaderChild></GoalHeaderChild>
+        <GoalHeaderChild>
+          <h3>Goal</h3>
+        </GoalHeaderChild>
+        <GoalHeaderChild>
+          {!!diff.length && (
+            <ShowDiffButton onClick={() => setShowDiff(!showDiff)}>
+              {showDiff ? "Hide" : "Show"} diff
+            </ShowDiffButton>
+          )}
+        </GoalHeaderChild>
+      </GoalHeader>
+      <GoalBox>{showDiff && diff.length ? createDiffText() : goalText}</GoalBox>
+    </React.Fragment>
+  );
+};
+
+export default GoalDisplay;
--- src/components/PlayPage/GameClient/LeftClient.js
+++ src/components/PlayPage/GameClient/LeftClient.js
@@ -1,43 +1,11 @@
 import React, { useState } from "react";
 import { PLAYER_STATES } from "./states";
 import VimClient from "./VimClient/VimClient";
+import GoalDisplay from "./GoalDisplay";
 import styled from "styled-components";
 import { Vim } from "react-vim-wasm";
 import vimOptions from "./VimClient/vimOptions";
 import PlayerStateIcon from "./PlayerStateIcon";
-
-const GoalBox = styled.code`
-  background-color: #212121;
-  border: 1px solid black;
-  border-radius: 3px;
-  font-family: "Lucida Console", "Consolas", monospace;
-  white-space: pre-wrap;
-  font-size: 9pt;
-  min-height: 300px;
-  width: 600px;
-  overflow: scroll;
-`;
-
-const GoalHeader = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-`;
-
-const GoalHeaderChild = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: center;
-  &:first-child {
-    margin-right: auto;
-    justify-content: flex-start;
-  }
-
-  &:last-child {
-    margin-left: auto;
-    justify-content: flex-end;
-  }
-`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -45,26 +13,6 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0px 25px;
-`;
-
-const ShowDiffButton = styled.button`
-  border: 4px double white;
-  background-color: transparent;
-  font-family: "Share Tech Mono", source-code-pro, Menlo, Monaco, Consolas,
-    "Courier New", monospace;
-  color: white;
-  cursor: pointer;
-  padding: 5px;
-  font-size: 12pt;
-  border-radius: 3px;
-`;
-
-const RedText = styled.span`
-  background-color: rgba(220, 50, 60, 0.6);
-`;
-
-const GreenText = styled.span`
-  background-color: rgba(50, 205, 50, 0.6);
 `;
 
 export const UserInfoHeader = styled.div`
@@ -109,25 +57,7 @@ export default function LeftClient({
     vimProps.files["/home/web_user/.vim/vimrc"] = user.vimrcText;
   }
 
-  const [showDiff, setShowDiff] = useState(false);
-
   const gameStarted = !!startText;
-
-  const createDiffText = () => {
-    return (
-      <React.Fragment>
-        {diff.map((token, index) => {
-          if (token.removed) {
-            return <RedText key={index}>{token.value}</RedText>;
-          }
-          if (token.added) {
-            return <GreenText key={index}>{token.value}</GreenText>;
-          }
-          return <span>{token.value}</span>;
-        })}
-      </React.Fragment>
-    );
-  };
 
   return (
     <Wrapper>
@@ -150,22 +80,9 @@ export default function LeftClient({
             handleKeystrokeReceived={handleKeystrokeReceived}
             removeKeystrokeListeners={removeKeystrokeListeners}
           ></VimClient>
+          <GoalDisplay diff={diff} goalText={goalText}></GoalDisplay>
         </React.Fragment>
       )}
-      <GoalHeader>
-        <GoalHeaderChild></GoalHeaderChild>
-        <GoalHeaderChild>
-          <h3>Goal</h3>
-        </GoalHeaderChild>
-        <GoalHeaderChild>
-          {!!diff.length && (
-            <ShowDiffButton onClick={() => setShowDiff(!showDiff)}>
-              {showDiff ? "Hide" : "Show"} diff
-            </ShowDiffButton>
-          )}
-        </GoalHeaderChild>
-      </GoalHeader>
-      <GoalBox>{showDiff && diff.length ? createDiffText() : goalText}</GoalBox>
     </Wrapper>
   );
 }
