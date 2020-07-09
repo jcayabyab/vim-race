8cdbdc475fa6a05c1289d6ce0e69c41bb300edc4
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-07-09 12:39:19 -0600

    add sign up button to landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,14 +1,22 @@
 import React from "react";
 import DemoClient from "./DemoClient/DemoClient";
+import { SolidAButton } from "../utils/SolidButton";
 
 export default function LandingPage() {
   return (
     <div>
       <DemoClient>
-        <p>Race against others to see who can edit text the fastest!</p>
         <p>
           To submit your solution, use the command <code>:E</code>.
         </p>
+        <p>
+          To race against others and customize your own <code>.vimrc</code>,
+          sign up for an account for free!
+        </p>
+        <div>
+
+        <SolidAButton href="/auth/google">Sign up for an account</SolidAButton>
+        </div>
       </DemoClient>
     </div>
   );
--- src/components/utils/SolidButton.js
+++ src/components/utils/SolidButton.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 // custom border
 const SolidButton = styled.button`
-  position: relative;
   border: 1px solid black;
   border-radius: 3px;
   background-color: #d1d1d1;
@@ -14,6 +13,21 @@ const SolidButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+export const SolidAButton = styled.a`
+  display: block;
+  border: 1px solid black;
+  border-radius: 3px;
+  background-color: #d1d1d1;
+  padding: 20px;
+  font-size: 16pt;
+  color: black;
+  cursor: pointer;
+  width: 100%;
+  box-sizing: border-box;
+  text-align: center;
+  text-decoration: none;
 `;
 
 export default SolidButton;
