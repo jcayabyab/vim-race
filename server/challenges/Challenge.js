763d31ac5b21740094a57e3a948c5a3fad76dc10
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-07-15 18:22:53 -0600

    add username to Challenge class

--- server/challenges/Challenge.js
+++ server/challenges/Challenge.js
@@ -1,9 +1,11 @@
 const { v4: uuidv4 } = require("uuid");
 
 class Challenge {
-  constructor(senderId, receiverId) {
+  constructor(senderId, senderUsername, receiverId, receiverUsername) {
     this.senderId = senderId;
+    this.senderUsername = senderUsername;
     this.receiverId = receiverId;
+    this.receiverUsername = receiverUsername;
     this.timestamp = new Date();
     this.uuid = uuidv4();
   }
