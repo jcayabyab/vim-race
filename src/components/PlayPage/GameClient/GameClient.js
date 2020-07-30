cf0fbd2edbb7de108739a3938180e9b32e4d869a
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-07-30 15:56:38 -0600

    add prompt to prevent leaving in middle of game

--- src/components/PlayPage/GameClient/GameClient.js
+++ src/components/PlayPage/GameClient/GameClient.js
@@ -5,6 +5,7 @@ import { GAME_STATES } from "./states";
 import styled from "styled-components";
 import { GameClientContext } from "./contexts/GameClientContext";
 import { GameClientSocketFunctionsContext } from "./contexts/GameClientSocketFunctionsContext";
+import { Prompt } from "react-router-dom";
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,8 +33,27 @@ export default function GameClient() {
     }
   }, [userInitialized, opponentInitialized, handleTerminalsLoaded, gameState]);
 
+  useEffect(() => {
+    const onBeforeUnload = (e) => {
+      e.preventDefault();
+      e.returnValue = "Leaving the game client will forfeit the game. Are you sure you want to leave?";
+    };
+
+    if (gameState !== GAME_STATES.IDLE) {
+      window.onbeforeunload = onBeforeUnload;
+    } else {
+      window.onbeforeunload = null;
+    }
+
+    return () => (window.onbeforeunload = null);
+  }, [gameState]);
+
   return (
     <Wrapper>
+      <Prompt
+        when={gameState !== GAME_STATES.IDLE}
+        message={"Leaving the game client will forfeit the game. Are you sure you want to leave?"}
+      ></Prompt>
       <LeftClient></LeftClient>
       <RightClient></RightClient>
     </Wrapper>
