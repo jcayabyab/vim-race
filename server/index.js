70ad7c32be85e5812da645cafe26bd2741218ed4
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-07-30 17:52:41 -0600

    move socket listeners to separate file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const http = require("http");
 const socketIo = require("socket.io");
 const MatchmakingClient = require("./matchmaking/MatchmakingClient");
 const ChallengesClient = require("./challenges/ChallengesClient");
-const { playerDict } = require("./matchmaking/PlayerDict");
 const passport = require("passport");
 const port = process.env.PORT || 4001;
 const bodyParser = require("body-parser");
@@ -40,51 +39,10 @@ app.use(authRoutes);
 app.use(profileRoutes);
 app.use(demoRoutes);
 
-io.on("connection", (socket) => {
-  // username of player - variables on a per-socket basis
-  let id = null;
+const getSocketInitializer = require("./socket");
+const initializeSocket = getSocketInitializer(challengesClient, matchmaker);
 
-  socket.on("handshake", (data) => {
-    console.log("client connected: ", data.id);
-    id = data.id;
-    if (playerDict.playerOnline(id)) {
-      const otherSocket = playerDict.getSocket(id);
-      playerDict.addPlayer(id, socket);
-      otherSocket.emit("login detected");
-    } else {
-      playerDict.addPlayer(id, socket);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log("client disconnected: " + id);
-    if (id) {
-      matchmaker.waitingQueue.removePlayerFromQueue(id);
-      const otherUsersAndChallenges = playerDict.handlePlayerDisconnect(id);
-      challengesClient.notifyOnRemoveOtherChallenges(otherUsersAndChallenges);
-    }
-    console.log(playerDict.dict);
-  });
-
-  // data: { id: String }
-  socket.on("request match", (data) => {
-    id = data.id;
-    if (matchmaker.playerIdActive(id)) {
-      console.log("user already connected as " + id);
-    } else {
-      // prevent user from connecting multiple times
-      console.log(data.id + " requested match");
-      // matchmaking logic
-      matchmaker.handleRequest(id, socket);
-    }
-  });
-
-  socket.on("cancel matchmaking", (data) => {
-    matchmaker.waitingQueue.removePlayerFromQueue(data.id);
-  });
-
-  challengesClient.addSocketListeners(socket);
-});
+io.on("connection", initializeSocket);
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
