cdf23667eb4da3988a6e2c30858b044b079ed091
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-08-06 14:32:37 -0600

    add play page username redirect

--- src/components/PlayPage/PlayPage.js
+++ src/components/PlayPage/PlayPage.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GameClient from "./GameClient/GameClient";
 import styled from "styled-components";
 import { GameClientStateProvider } from "./GameClient/contexts/GameClientContext";
 import { SocketProvider } from "./GameClient/contexts/SocketContext";
 import { GameClientSocketFunctionsProvider } from "./GameClient/contexts/GameClientSocketFunctionsContext";
 import { ChallengesProvider } from "./GameClient/contexts/ChallengesContext";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const ClientComponent = styled.div`
   max-width: 1400px;
@@ -13,7 +15,19 @@ const ClientComponent = styled.div`
   justify-content: space-between;
 `;
 
+const useRedirectOnNoUsername = () => {
+  const history = useHistory();
+  const user = useSelector(({ user }) => user);
+  useEffect(() => {
+    if (user && !user.username) {
+      history.push("/settings");
+    }
+  }, [user, history]);
+};
+
 export default function Play() {
+  useRedirectOnNoUsername();
+
   return (
     <ClientComponent>
       <SocketProvider>
