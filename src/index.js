50c192a9ec72668dcd5e238a2f439ba5020a37ff
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-08-17 16:03:43 -0600

    Revert "Remove react-snap functionality"
    This reverts commit a30f469f85998da73d62d9fbd8d0979dcca3489a.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -15,7 +15,12 @@ const Index = () => (
   </Provider>
 );
 
-ReactDOM.render(<Index></Index>, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Index></Index>, rootElement);
+} else {
+  render(<Index></Index>, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
