e424b7cf6674aa0604d0bcfe97890861eeb33e17
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-08-22 16:33:50 -0600

    update changelog

--- src/components/AboutPage/ChangelogPage.js
+++ src/components/AboutPage/ChangelogPage.js
@@ -22,6 +22,10 @@ export default function ChangelogPage() {
         <li>Rating system</li>
       </ul>
       <h1>Changelog</h1>
+      <h2>August 22nd, 2020</h2>
+      <ul>
+        <li>Fixed a bug where pressing space scrolls the page down.</li>
+      </ul>
       <h2>August 18th, 2020</h2>
       <ul>
         <li>Fixed a bug where a user's sent challenges weren't being cancelled when they enter a new match</li>
